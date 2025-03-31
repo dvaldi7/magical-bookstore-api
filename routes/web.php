e45f8a4886e47d4592c4573c2392e5aa71c4ddeb
Mario Moreno <cursotecnoweb@gmail.com>
2025-03-31 18:05:09 +0200

    feat: create-end-point-to-get-all-the-books

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,4 @@
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BookController::class, 'index']);
