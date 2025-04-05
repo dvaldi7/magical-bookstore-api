3df1153d56d860fb7402b0c051cf6c4a4dcf60e1
Mario Moreno <cursotecnoweb@gmail.com>
2025-04-05 22:14:24 +0200

    feat: improve routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [BookController::class, 'index']);
+Route::get('/books', [BookController::class, 'index']);
 Route::get('/books/author', [BookController::class, 'searchByAuthor']);
 Route::get('/books/title', [BookController::class, 'searchByTitle']);
 Route::get('/books/{id}', [BookController::class, 'show']);
