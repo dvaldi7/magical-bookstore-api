49de70f92ca2f47063b4264ea7f2caee862e94f5
Mario Moreno <cursotecnoweb@gmail.com>
2025-04-09 21:34:40 +0200

    ops: create Questions table and run migration

---
+++ database/migrations/2025_04_09_185750_create_table_questions.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('question', 255);
+            $table->string('answer', 100);
+            $table->timestamps();
+        });
+
+        // import data from JSON questions file
+        $json = file_get_contents(database_path('../Application/MariaDB/questions.json'));
+        $questions = json_decode($json, true);
+
+        foreach ($questions['questions'] as $question) {
+            DB::table('questions')->insert([
+                'question' => $question['question'],
+                'answer' => $question['answer'],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
