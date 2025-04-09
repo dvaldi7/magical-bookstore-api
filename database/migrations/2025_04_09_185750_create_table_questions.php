ac3917f5e7b87c868272279df02a308a1b4e9ed0
Mario Moreno <cursotecnoweb@gmail.com>
2025-04-09 21:41:49 +0200

    ops: change bigint to int field

--- database/migrations/2025_04_09_185750_create_table_questions.php
+++ database/migrations/2025_04_09_185750_create_table_questions.php
@@ -13,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('questions', function (Blueprint $table) {
-            $table->id();
+            $table->integer('id');
             $table->string('question', 255);
             $table->string('answer', 100);
             $table->timestamps();
